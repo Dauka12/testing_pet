141aa29212533eee00c2542f9884ce8059cf3446
Dauka12 <dauka2707@gmail.com>
2025-03-29 16:36:47 +0500

    clear

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,16 @@
 import react from '@vitejs/plugin-react';
+import { visualizer } from 'rollup-plugin-visualizer';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-    plugins:[react()],
+    plugins:[
+        react(),
+        visualizer({
+            open: true,
+            gzipSize: true,
+            brotliSize: true,
+        }),
+    ],
     assetsInclude: ['**/*.docx', '**/*.xlsx'],
     server: {
         port: 3000
